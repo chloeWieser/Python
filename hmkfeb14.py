d506666a644a66a7c9b0b17b0661932eaae6fc4a
chloeWieser <chloerosewieser@gmail.com>
2022-02-14 17:15:55 -0500

    hmkfeb14

---
+++ hmkfeb14.py
@@ -0,0 +1,71 @@
+# HMK FEB14 FUNCTIONS
+
+# 1. Find the smallest number
+# Write a function smallest that accepts a List of numbers as an argument.
+
+# It should return the smallest number in the List.
+
+# nums = [2, 3, 1, 3, 4, 5, 6]
+# def smallest():
+#     min = nums[0]
+#     for num in nums:
+#         if (min > num):
+#             min = num
+        
+#     return min
+# print(smallest())
+
+# 2. Find the largest number
+# Write a function largest that accepts a List of numbers as an argument.
+
+# It should return the largest number in the List.
+
+# nums = [2, 3, 1, 3, 4, 5, 6]
+# def largest(numbersList):
+#     max = 0
+#     for num in numbersList:
+#         if (max < num):
+#             max = num
+#     return max
+
+            
+# print(largest(nums))
+
+# 3. Find the shortest String
+# Write a function shortest that accepts a List of Strings as an argument.
+
+# It should return the shortest String in the List.
+
+# pets = ["hamster", "cat", "dogs", "fish"]
+# def shortest(stringList):  #in this case the 'argument name' is really called a parameter
+#     min = stringList[0]
+#     for pet in stringList:
+#         if (len(min) > len(pet)):
+#             min = pet
+            
+#     return min
+# print(shortest(pets))
+
+# 4. Find the longest String
+# Write a function longest that accepts a List of Strings as an argument.
+
+# It should return the longest String in the List.
+
+# pets = ["cat", "dogs", "fish", "hamster"]
+# def longest(stringList):  #in this case the 'argument name' is really called a parameter
+#     max = stringList[0]
+#     for pet in stringList:
+#         if (len(max) < len(pet)):
+#             max = pet
+            
+#     return max
+# print(longest(pets))
+
+
+
+
+
+
+
+
+
